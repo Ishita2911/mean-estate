5f08363874a622a5973fcd2be544654ef0f3067d
Ishita Jain <jainishita29@gmail.com>
2025-02-20 20:51:48 +0530

    sign up page

--- api/index.js
+++ api/index.js
@@ -13,3 +13,9 @@ app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });
 
+
+app.get('/',(req,res)=>{
+    res.json({message:'Hello World'});
+});
+
+
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function SignUp() {
   return (
-    <div>
-        SignUp
+    <div className='w-1/3 mx-auto mt-20'> 
+        <h1 className='text-3xl text-center font-semibold my-7'> Sign up</h1>
+        <form className='flex flex-col gap-4'>
+          <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' />
+          <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' />
+          <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' />
+          <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
+        </form>
+      <div className='flex mt-5 gap-3'>
+      <p>Have an account?</p>
+      <p className='text-blue-400'>Sign in</p>
+     </div>
     </div>
   )
 }
