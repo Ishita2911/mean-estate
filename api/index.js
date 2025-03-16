88e95d28a2f119fcdc4bb2aa9ad84c1dd15e1e73
Ishita Jain <jainishita29@gmail.com>
2025-03-16 22:44:13 +0530

    sign up changes

--- api/index.js
+++ api/index.js
@@ -33,4 +33,16 @@ mongoose.connect(process.env.MONGO).then(()=>{
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message= err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+            success:false,
+            statusCode,
+            message,
+
+        });
+    });
+
+
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,18 +1,43 @@
 import React from 'react'
 
-export default function SignUp() {
+export default function SignUp() 
+{
+  const {formdata, setformdata}=useState({})
+  const handlechange=(e)=>{
+    setformdata(
+      {
+        ...formdata,
+        [e.target.id]:e.target.value
+      });
+  };
+
+  const handlesubmit=async(e)=>{ 
+    e.preventDefault();
+    const res= await fetch('/api/auth/signup', 
+      {
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body:JSON.stringify(formdata),
+      });
+      const data=await res.json();
+      console.log(data);
+  };
   return (
     <div className='w-1/3 mx-auto mt-20'> 
         <h1 className='text-3xl text-center font-semibold my-7'> Sign up</h1>
         <form className='flex flex-col gap-4'>
-          <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' />
-          <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' />
-          <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' />
+          <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handlechange}/>
+          <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email'onChange={handlechange} />
+          <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password'onChange={handlechange} />
           <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
         </form>
       <div className='flex mt-5 gap-3'>
       <p>Have an account?</p>
-      <p className='text-blue-400'>Sign in</p>
+      <Link to={'/sign-in'}>
+      <span className='text-blue-400'>Sign in</span >
+      </Link>
      </div>
     </div>
   )
